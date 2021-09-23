5c6d12433b83c68fca7567c5aa1ec11e090777c4
Joao Otavio <joaolima.felipelli@gmail.com>
2021-09-23 10:40:54 -0300

    Commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RodaPeComponent } from './roda-pe/roda-pe.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RodaPeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
